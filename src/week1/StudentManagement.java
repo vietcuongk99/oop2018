04d6e504f06f590e9f3819b2fbb86d73f5cc8336
vietcuongk99 <daoquocthai8@gmail.com>
2018-09-19 21:15:24 +0700

    update

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -6,7 +6,6 @@ public class StudentManagement {
     Student[] arr = new Student[100];
 
 
-
     public static boolean sameGroup(Student s1, Student s2) {
         // TODO:
         if(s1.getGroup().equals(s2.getGroup()))
@@ -21,24 +20,25 @@ public class StudentManagement {
 
     void removeStudent(String id) {
         // TODO:
+
     }
 
     public static void main(String[] args) {
         // TODO:
-
         Student s = new Student();
+        Student s0 = new Student();
         Student s1 = new Student();
         Student s2 = new Student();
 
-        //Student s1 = new Student("Dinh Viet Cuong", "17020624", "uet@vnu.edu.vn");
+        Student s3 = new Student("Dinh Viet Cuong", "17020624", "uet@vnu.edu.vn");
 
-        //Student s2 = new Student(s1);
+        Student s4 = new Student(s3);
 
 
-        s.setName("Nguyen Van A");
-        s.setID("17020624");
-        s.setGroup("INT22041");
-        s.setEmail("uetmail@vnu.edu.vn");
+        s0.setName("Nguyen Van A");
+        s0.setID("17020624");
+        s0.setGroup("INT22041");
+        s0.setEmail("uetmail@vnu.edu.vn");
 
         s1.setName("Nguyen Van B");
         s1.setID("17020625");
@@ -50,15 +50,15 @@ public class StudentManagement {
         s2.setGroup("INT22042");
         s2.setEmail("uetmail@vnu.edu.vn");
 
-        /*
-        s.getInfo();
-        s1.getInfo();
-        s2.getInfo();
-        */
 
-        System.out.println("Compare s1 and s2 class: " + sameGroup(s1,s2));
-        System.out.println("Compare s and s2 class: " + sameGroup(s,s2));
-        System.out.println("Compare s1 and s class: " + sameGroup(s1,s));
+        s.getInfo();
+        s3.getInfo();
+        s4.getInfo();
+
+
+        System.out.println("Compare s1 and s2 group: " + sameGroup(s1,s2));
+        System.out.println("Compare s0 and s2 group: " + sameGroup(s0,s2));
+        System.out.println("Compare s1 and s0 group: " + sameGroup(s1,s0));
 
     }
 }
