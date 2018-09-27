bb6ae910510534160ce74b7b88783587235c9d8c
vietcuongk99 <43227743+vietcuongk99@users.noreply.github.com>
2018-09-27 10:19:28 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,9 +1,12 @@
 package week2.task1;
+
 import java.util.Scanner;
 public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
+        a = Math.abs(a);
+        b = Math.abs(b);
         if (b==0)
             return a;
 
