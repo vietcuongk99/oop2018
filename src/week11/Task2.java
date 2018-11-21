7730e402fdc458574614239594a31051e9139f96
vietcuongk99 <43227743+vietcuongk99@users.noreply.github.com>
2018-11-21 23:09:11 +0700

    up2

--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -4,22 +4,36 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Task2 {
-    public static <T extends Comparable> void getMax(ArrayList<T> list) {
-        System.out.println(Collections.max(list));
+//     public static <T extends Comparable> void getMax(ArrayList<T> list) {
+//         System.out.println(Collections.max(list));
 
+//     }
+
+
+//     public static void main(String args[]) {
+
+//         ArrayList<String> al = new ArrayList<>();
+//         al.add("Cuong");
+//         al.add("Long");
+//         al.add("Dong");
+
+//         getMax(al);
+//     }
+    public static <T extends Comparable> T getMax(T[] a) {
+       
+        T max = a[0];
+        for (int i = 1; i < a.length; i++) {
+
+            if (max.compareTo(a[i]) < 0) max = a[i];
+        }
+        return max;
     }
-
 
     public static void main(String args[]) {
 
-        ArrayList<String> al = new ArrayList<>();
-        al.add("Cuong");
-        al.add("Long");
-        al.add("Dong");
+        String[] al = {"Cuong","dong","long"};
 
-        getMax(al);
-
-
+        System.out.println(getMax(al));
 
     }
 }
