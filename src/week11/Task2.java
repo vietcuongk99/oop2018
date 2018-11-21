c5d9e9a88f009e90ea50b7a98b562a18da4a7751
vietcuongk99 <43227743+vietcuongk99@users.noreply.github.com>
2018-11-21 21:59:12 +0700

    up

--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,25 @@
 package week11;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Task2 {
+    public static <T extends Comparable> void getMax(ArrayList<T> list) {
+        System.out.println(Collections.max(list));
+
+    }
+
+
+    public static void main(String args[]) {
+
+        ArrayList<String> al = new ArrayList<>();
+        al.add("Cuong");
+        al.add("Long");
+        al.add("Dong");
+
+        getMax(al);
+
+
+
+    }
 }
