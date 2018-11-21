b417253d40384b16d0f8fb6814d319cb541d5872
vietcuongk99 <43227743+vietcuongk99@users.noreply.github.com>
2018-11-21 21:58:45 +0700

    up

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,31 @@
 package week11;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 public class Task1 {
-}
+
+
+    public static <T extends Comparable> void sort(ArrayList<T> list) {
+        Collections.sort(list);
+        for(T a: list) {
+            System.out.println(a);
+        }
+    }
+
+
+    public static void main(String args[]) {
+
+        ArrayList<String> al = new ArrayList<>();
+        al.add("Cuong");
+        al.add("Long");
+        al.add("Dong");
+
+        sort(al);
+
+
+        }
+    }
+
+
