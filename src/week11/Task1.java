fd59dd151bccd62ca45d10f3d8ab9bf6962c99f9
vietcuongk99 <43227743+vietcuongk99@users.noreply.github.com>
2018-11-21 23:07:17 +0700

    up2

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -7,25 +7,37 @@ import java.util.Collections;
 public class Task1 {
 
 
-    public static <T extends Comparable> void sort(ArrayList<T> list) {
-        Collections.sort(list);
-        for(T a: list) {
-            System.out.println(a);
-        }
-    }
+//     public static <T extends Comparable> void sort(ArrayList<T> list) {
+//         Collections.sort(list);
+//         for(T a: list) {
+//             System.out.println(a);
+//         }
+//     }
+    public static <T extends Comparable> void sort(T[] a) {
 
+            for(int i = 0 ; i < a.length - 1 ; i++){
+                for (int j = 0 ; j < a.length - 1  ; j++){
+                    if(a[j].compareTo(a[j+1])>0){
+                        T temp = a[j];
+                        a[j] = a[j+1];
+                        a[j+1] = temp;
+                    }
+
+                }
+            }
+    }
 
     public static void main(String args[]) {
 
-        ArrayList<String> al = new ArrayList<>();
-        al.add("Cuong");
-        al.add("Long");
-        al.add("Dong");
+        Integer[] al = {10,5,6,6,1};
 
         sort(al);
-
-
+        for(int i = 0; i<al.length; i++) {
+            System.out.println(al[i]);
         }
+
     }
+}
+
 
 
